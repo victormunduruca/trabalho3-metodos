46a36a803a3e3f6d4e2bacfd22689c1116245c66
Victor Munduruca <victormunduruca@gmail.com>
2018-01-10 19:39:23 -0300

    Finalizando

--- trabalho3.m
+++ trabalho3.m
@@ -1,17 +1,17 @@
 %Matheus Teixeira dos Santos e Victor Munduruca Gomes Santos
-%EXA862 - Métodos Numéricos
-%Departamento de Ciências Exatas
+%EXA862 - M?todos Num?ricos
+%Departamento de Ci?ncias Exatas
 %Universidade Estadual de Feira de Santana
-
-#x = [0, 1, 2, 3, 4, 5];
-#m = 2;
-#y = [2.1 7.7 13.6 27.2 40.9 61.1];
+format shortEng
+% x = [0, 1, 2, 3, 4, 5];
+% m = 2;
+% y = [2.1 7.7 13.6 27.2 40.9 61.1];
 
 %Amostras x e y.
 x = [50, 80, 130, 200, 250, 350, 450, 550, 700];
 y = [99, 177, 202, 248, 229, 219, 173, 142, 72];
 
-%Ordem do polinômio desejado
+%Ordem do polin?mio desejado
 m = 3;
 
 regressaoPolinomial(x, y, m);
